d5938719ff21137b39aca5ca5c34165781a90e3c
Haingocpham19 <haingocpham@hotmail.com>
2022-04-20 00:23:18 +0700

    aaaa

---
+++ public/js/custom.js
@@ -0,0 +1,73 @@
+//click ontop
+var fixed = document.querySelector('.fixed');
+var mybutton = document.getElementById("back-to-top");
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    fixed.style.boxShadow = "0 0 6px #2c1818";
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+    fixed.style.boxShadow = "none";
+  }
+}
+// Bind your button click, scroll direction and effect speed
+document.getElementById("back-to-top").onclick = function() {
+  scrollTo(0, 200); // it will take 8 seconds to reach to top.
+}
+
+// Element to move, time in ms to animate
+function scrollTo(element, duration) {
+  var e = document.documentElement;
+  if (e.scrollTop === 0) {
+    var t = e.scrollTop;
+    ++e.scrollTop;
+    e = t + 1 === e.scrollTop-- ? e : document.body;
+  }
+  scrollToC(e, e.scrollTop, element, duration);
+}
+
+// Element to move, element or px from, element or px to, time in ms to animate
+function scrollToC(element, from, to, duration) {
+  if (duration <= 0) return;
+  if (typeof from === "object") from = from.offsetTop;
+  if (typeof to === "object") to = to.offsetTop;
+
+  scrollToX(element, from, to, 0, 1 / duration, 20, easeOutCuaic);
+}
+
+function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+  if (t01 < 0 || t01 > 1 || speed <= 0) {
+    element.scrollTop = xTo;
+    return;
+  }
+  element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+  t01 += speed * step;
+  setTimeout(function() {
+    scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+  }, step);
+}
+
+function easeOutCuaic(t) {
+  t--;
+  return t * t * t + 1;
+}
+//click show menu mobile
+document.getElementById("btn-more-ne").addEventListener("click", function(e) {
+    let check = document.querySelector('.more-menu').style.display;
+    if(check==="flex"){
+      ShowHideMenuMobile();
+    }
+    else{
+      ShowMoreMenuMobile();
+    }
+})
+function ShowMoreMenuMobile(){
+  document.querySelector('.more-menu').setAttribute("style","display:flex !important;justify-content:space-around !important;");
+}
+function ShowHideMenuMobile(){
+  document.querySelector('.more-menu').setAttribute("style","display:none !important;");
+}
